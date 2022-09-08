6d6af930567c264456b18bd622d2e4234c091908
Mohamed Salah <m.salah@mu.edu.eg>
2022-09-08 09:27:33 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * The function of this c code is to exactly print "Programming is like building a multilingual puzzle"
+ * followed by a new line 
+ * the program should return 0 after excusion
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
