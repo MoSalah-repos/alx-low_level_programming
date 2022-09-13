e89ab10191888e3581d5f9206d946ba16b397228
Mohamed Salah <m.salah@mu.edu.eg>
2022-09-13 11:28:08 -0700

    Task 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add -  adds two integers and returns the result.
+ * @x: the first number
+ * @y: the second number
+ * Return: result of summation
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
