b15910c02f1885d6e01c17c25701ef8f4b320905
Mohamed Salah <m.salah@mu.edu.eg>
2022-09-13 10:15:01 -0700

    Task 7. the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,16 @@
  */
 int print_last_digit(int num)
 {
+	int last;
+
 	if (num < 0)
 		num = -num;
-	_putchar((num % 10) + '0');
-	return (num % 10);
+
+	last = num % 10;
+
+	if (last < 0)
+		last = -last;
+
+	_putchar(last + '0');
+	return (last);
 }
