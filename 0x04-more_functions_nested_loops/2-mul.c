f3f644794c650e4a374e68c375b254b98debabfc
Mohamed Salah <m.salah@mu.edu.eg>
2022-09-15 04:09:54 -0700

    Task 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  mul - a function that multiplies two integers
+ * @num1: defines the first number
+ * @num2: sdefines the seecond number
+ * Return: multiplication result of the two numbers
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
