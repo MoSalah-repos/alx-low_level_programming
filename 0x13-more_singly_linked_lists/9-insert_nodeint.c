7393e1d9e5d1b861c43d1e5eb6e8ec9012413e94
Mohamed Salah <m.salah@mu.edu.eg>
2022-10-25 19:49:58 -0700

    Task 9. inserts a new node at a given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert a new node at a given position
+ * @head: double pointer to head
+ * @index: insert node at this index, starting count at 0
+ * @n: value to store in node
+ *
+ * Return: Address of new node or NULL if failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+{
+	listint_t *new;
+	listint_t *tmp = *head;
+	unsigned int count;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (count = 1; tmp && count < index; count++)
+	{
+		tmp = tmp->next;
+		if (tmp == NULL)
+			return (NULL);
+	}
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+
+	if (index == 0)
+	{
+		*head = new;
+		new->next = tmp;
+	}
+	else if (tmp->next)
+	{
+		new->next = tmp->next;
+		tmp->next = new;
+	}
+	else
+	{
+		new->next = NULL;
+		tmp->next = new;
+	}
+
+	return (new);
+}
